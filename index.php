10979c2a044820f7d0a795e4f967d7b223bea83b
antspring <max.verzilov32@gmail.com>
2023-03-10 04:52:29 +0500

    feat: json to xml parser

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
 require_once 'vendor/autoload.php';
+
+use App\Parser;
+use App\Parsers\Json;
+
+$parser = new Parser(new Json('src\files\test.json'));
+$parser->init();
+$parser->toXML();
---
+++ src/Parsers/Json.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Parsers;
+
+class Json extends FormatParser
+{
+    public function init():void
+    {
+        $this->content = file_get_contents($this->path);
+        $this->data = json_decode($this->content, true);
+    }
+
+    public function toXml():void
+    {
+        $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><datalist></datalist>');
+
+        $this->arrayToXMl($this->data, $xml);
+
+        $name = basename($this->path);
+
+        $xml->asXML("src/files/{$name}.xml");
+    }
+
+    private function arrayToXml(array $array, &$xml):void
+    {
+        foreach ($array as $key => $value){
+            if (is_int($key)){
+                $key = 'Element'.$key;
+            }
+            if (is_array($value)){
+                $label = $xml->addChild($key);
+                $this->arrayToXMl($value, $label);
+            }
+            else {
+                $xml->addChild($key, $value);
+            }
+        }
+    }
+}
